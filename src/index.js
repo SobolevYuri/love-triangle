4d369a2b5c0517e92dc2bd5e07c69db30f907d73
SobolevYuri <yura.sobolev.91@gmail.com>
2018-02-21 02:05:18 +0300

    Second task by Sobolev

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+
+let arr_len=preferences.length, triangCnt=0, arr=preferences;
+
+for(let j=0;j<arr_len;j++)
+{
+  if((arr[arr[j]-1]-1 != arr[j]-1) && (arr[arr[arr[j]-1]-1]-1) == j)
+  {
+triangCnt++;
+  }
+}
+
+return (triangCnt/3);
 };
